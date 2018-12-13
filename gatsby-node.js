d75364fee8e84dac3071efa5e9d6ae8bac0fdb08
Govind Karki <govind050796@gmail.com>
2018-12-13 18:17:52 +0530

    Add fetching of next and prev nodes for projects and blogs

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,10 +41,34 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const result = await graphql(`
     {
-      allMarkdownRemark {
+      allBlogs: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/_posts/" } }
+        sort: { fields: [frontmatter___date], order: DESC }
+      ) {
         edges {
           node {
-            fileAbsolutePath
+            frontmatter {
+              title
+              author {
+                id
+              }
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+      allProjects: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/projects/" } }
+        sort: { fields: [frontmatter___date], order: DESC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              description
+            }
             fields {
               slug
             }
@@ -62,25 +86,44 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    const re = /content\/_posts/
-    if (re.test(node.fileAbsolutePath)) {
-      createPage({
-        path: node.fields.slug,
-        component: path.resolve(`./src/templates/blog-post.js`),
-        context: {
-          slug: node.fields.slug,
-        },
-      })
-    } else {
-      createPage({
-        path: node.fields.slug,
-        component: path.resolve(`./src/templates/project-page.js`),
-        context: {
-          slug: node.fields.slug,
-        },
-      })
-    }
+  result.data.allBlogs.edges.forEach(({ node }, index) => {
+    // To Get the next and previous blog.
+    let next = index === 0 ? null : result.data.allBlogs.edges[index - 1].node
+
+    const prev =
+      index === result.data.allBlogs.edges.length - 1
+        ? null
+        : result.data.allBlogs.edges[index + 1].node
+
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        slug: node.fields.slug,
+        next,
+        prev,
+      },
+    })
+  })
+  result.data.allProjects.edges.forEach(({ node }, index) => {
+    // To Get the next and previous project.
+    let next =
+      index === 0 ? null : result.data.allProjects.edges[index - 1].node
+
+    const prev =
+      index === result.data.allProjects.edges.length - 1
+        ? null
+        : result.data.allProjects.edges[index + 1].node
+
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/project-page.js`),
+      context: {
+        slug: node.fields.slug,
+        next,
+        prev,
+      },
+    })
   })
   result.data.allAuthorYaml.edges.forEach(({ node }) => {
     createPage({
