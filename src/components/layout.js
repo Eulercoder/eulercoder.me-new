36b8fd736fe5844bcd350afdc68d0fc9a23a72fd
Govind Karki <govind050796@gmail.com>
2018-12-17 13:06:56 +0530

    reduce padding-top

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children, background, style }) => (
             margin: '0 auto',
             maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
-            paddingTop: '1.45rem',
+            paddingTop: '5rem',
             ...style,
           }}
         >
