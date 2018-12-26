84e0095b301320d8e637e8e1104cb4ae6c87a803
Karan Bathija <40559630+Karan064@users.noreply.github.com>
2018-12-26 12:09:00 +0530

    changed back to staticquery

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,68 +1,43 @@
 import React from 'react'
-//import {  graphql } from 'gatsby'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 import Header from './header'
 import './layout.css'
 
 
 
-//const Layout = ({ children, background, style }) => (
-//  <StaticQuery
-//    query={graphql`
-//      query SiteTitleQuery {
-//        site {
-//          siteMetadata {
-//            title
-//          }
-//        }
-//      }
-//    `}
-//    render={data => (
-//      <div style={{ background, minHeight: '100vh' }}>
-//        <Header siteTitle={data.site.siteMetadata.title} />
-//        <div
-//          style={{
-//            margin: '0 auto',
-//            maxWidth: 960,
-//            padding: '0px 1.0875rem 1.45rem',
-//            paddingTop: '5rem',
-//            ...style,
-//          }}
-//        >
-//          {children}
-//        </div>
-//      </div>
-//    )}
-//  />
-//)
-
 const Layout = ({ children, background, style }) => (
-  <div style={{ background, minHeight: '100vh' }}>
-  <Header siteTitle= "Eulercoder" />
-  <div
-    style={{
-      margin: '0 auto',
-      maxWidth: 960,
-      padding: '0px 1.0875rem 1.45rem',
-      paddingTop: '5rem',
-      ...style,
-    }}
-  >
-    {children}
-  </div>
-</div>
- )
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div style={{ background, minHeight: '100vh' }}>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: '5rem',
+            ...style,
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    )}
+  />
+)
 
-//export const query = graphql`
-//query {
-//  site {
-//    siteMetadata {
-//      title
-//    }
-//  }
-//}
-//`
-// Layout.propTypes = {
-//   children: PropTypes.node.isRequired,
-// }
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
 
 export default Layout
