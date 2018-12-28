8b4fcc01599a9da1f2dc7c2bc06fa81953e44763
Karan Bathija <40559630+Karan064@users.noreply.github.com>
2018-12-28 13:53:33 +0530

    added subscribe box below blog

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -129,16 +129,6 @@ const Right = styled(StyledNext)`
     margin-right: -24px;
   }
 `
-
-const Footer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: rgb(102,51,153);
-  background: linear-gradient(207deg, rgba(102,51,153,0.9) 10%, rgba(81,24,138,1) 73%);
-  color: white;
-  font-family: 'Roboto', sans-serif;
-`
-
 const Subscribe = styled.div`
   padding: 60px 0;
   display: flex;
@@ -218,6 +208,75 @@ const Subscribe = styled.div`
   }
 `
 
+const SubscribeBottom = styled(Subscribe)`
+  
+    & input {
+      height: 44px;
+      font-size: 18px;
+      padding: 0px;
+      padding-left: 10px;
+      color: rebeccapurple;
+      width: 340px;
+      margin-right: 10px;
+      border: 2px solid rgba(81,24,138,0.6);
+      border-radius: 30px;
+      box-sizing: border-box;
+      transition: 0.5s transform cubic-bezier(0.215, 0.61, 0.355, 1);
+      outline: none;
+      box-shadow: 0 16px 32px rgba(0, 0, 0, 0.08);
+      &:hover,
+      &:focus {
+        box-shadow: 0 16px 32px rgba(0, 0, 0, 0.08),
+          0 6px 12px rgba(0, 0, 0, 0.08);
+        transform: translateY(-3px);
+      }
+    }
+
+
+    & button {
+      font-size: 18px;
+      height: 44px;
+      cursor: pointer;
+      padding: 0 10px;
+      border: none;
+      background: linear-gradient(207deg, rgba(102,51,153,0.9) 10%, rgba(81,24,138,1) 73%);
+      outline: none;
+      box-sizing: border-box;
+      color: white;
+      transition: 0.5s transform cubic-bezier(0.215, 0.61, 0.355, 1);
+      box-shadow: 0 16px 32px rgba(0, 0, 0, 0.08);
+      &:hover,
+      &:focus {
+        box-shadow: 0 16px 32px rgba(0, 0, 0, 0.08),
+          0 6px 12px rgba(0, 0, 0, 0.08);
+        transform: translateY(-3px);
+      }
+    }
+
+    @media (max-width: 1024px) {
+  
+      flex-direction:column;
+      
+    
+      & input {
+        width: 200px;
+      }
+  
+      & button {
+        padding: 0px 5px;
+      }
+      & h3 {
+        margin: 10px;
+      }
+      & h4 {
+        text-align:center;
+        margin: 10px;
+        font-weight: normal;
+      }
+    
+    }
+`
+
 
 const Blogs = ({ data, pageContext: { prev, next } }) => {
   const post = data.markdownRemark
@@ -264,19 +323,14 @@ const Blogs = ({ data, pageContext: { prev, next } }) => {
           )}
         </Navigation>
       </Container>
-    </Layout>
-    <Footer>
-      <Subscribe>
-        <div>
-          <h4>{'Eulercoder Newsletter'.toUpperCase()}</h4>
-          <h3>Subscribe for all the latest posts</h3>
-        </div>
+      <SubscribeBottom>
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <input type="text" id="subscription-email" placeholder="your@email.com" />
           <button>Subscribe</button>
         </div>
-      </Subscribe>
-    </Footer>
+      </SubscribeBottom>
+    </Layout>
+  
     </>
   )
 }
