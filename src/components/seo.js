52f014e1630e9b62c24614713bff248b508d5a24
Karan Bathija <40559630+Karan064@users.noreply.github.com>
2019-01-03 10:45:50 +0530

    fix for site title

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,7 +5,7 @@ import { StaticQuery, graphql } from 'gatsby'
 
 function SEO({ description, lang, meta, keywords, title }) {
  // Helmet.canUseDOM = false;
-  const helmet = Helmet.renderStatic();
+  const helmet = Helmet.peek();
   helmet.title = `${title}`;
   console.log("rewind "+JSON.stringify(helmet));
   return (
